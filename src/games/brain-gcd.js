acc5ceaacdd7b926a82f07c6a50628e6c815c625
Sergey Belokrylets <s.belokrylets@gmail.com>
2021-08-02 13:58:46 +0300

    fix: исправлен файл brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -2,18 +2,12 @@ import { random } from './index.js';
 
 export const task = 'Find the greatest common divisor of given numbers.';
 
-export const correct = (num) => {
-  const arr = num.split(' ');
+export const correct = (numbers) => {
+  const arrNumbers = numbers.split(' ');
+  arrNumbers[0] = Number(arrNumbers[0]);
+  arrNumbers[1] = Number(arrNumbers[1]);
   let result = 0;
-  const [num1, num2] = arr;
-  if (num1 > num2) {
-    for (let i = num2; i >= 1; i -= 1) {
-      if (num1 % i === 0 && num2 % i === 0) {
-        result = i;
-        break;
-      }
-    }
-  }
+  const [num1, num2] = arrNumbers;
   if (num1 < num2) {
     for (let i = num1; i >= 1; i -= 1) {
       if (num1 % i === 0 && num2 % i === 0) {
@@ -30,6 +24,6 @@ export const correct = (num) => {
 
 export const gcd = () => {
   const num1 = random(1, 10);
-  const num2 = random(1, 10);
+  const num2 = random(10, 20);
   return `${num1} ${num2}`;
 };
